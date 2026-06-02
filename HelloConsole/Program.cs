7dc469bba9464784088c2243c290ca4cb233b568
loic-job-dev <loicjobard@hotmail.com>
2026-06-02 13:35:26 +0200

    added a loop in the main program, refacto with switch/case

--- HelloConsole/Program.cs
+++ HelloConsole/Program.cs
@@ -16,66 +16,92 @@ class Program
         
         MonsterService service = new MonsterService(client, cache);
 
-        Console.WriteLine(
-            "Choisissez un mode de consultation :\n" +
-            "1 - Par index\n" +
-            "2 - Par nom de monstre"
-        );
-
-        if (!int.TryParse(
-                Console.ReadLine(),
-                out int choice))
-        {
-            Console.WriteLine("Nombre invalide");
-            return;
-        }
-
-        if (choice == 1)
+        int choice = 0;
+        
+        while (choice != 9)
         {
             Console.WriteLine(
-                "Choisissez un index de monstre à consulter :"
+                "\nChoisissez un mode de consultation :\n" +
+                "1 - Par index\n" +
+                "2 - Par nom de monstre\n" +
+                "9 - Quitter"
             );
 
             if (!int.TryParse(
                     Console.ReadLine(),
-                    out int index))
+                    out choice))
             {
                 Console.WriteLine("Nombre invalide");
                 return;
             }
 
-            try
+            switch (choice)
             {
-                Monster monster = service.GetMonsterByIndex(index)
-                    .GetAwaiter()
-                    .GetResult();
-                monster.DisplayInfos();
-            }
-            catch (HttpRequestException ex)
-            {
-                ConsoleHelper.WriteLineColor("Index inconnu", ConsoleColor.DarkRed);
-            }
-        }
+                case 1:
+                {
+                    Console.WriteLine("Choisissez un index de monstre à consulter :");
 
-        if (choice == 2)
-        {
-            Console.WriteLine(
-                "\nChoisissez un nom de monstre à consulter :"
-            );
+                    if (!int.TryParse(
+                            Console.ReadLine(),
+                            out int index))
+                    {
+                        Console.WriteLine(
+                            "Nombre invalide");
+                        break;
+                    }
 
-            string name = Console.ReadLine();
+                    try
+                    {
+                        Monster monster =
+                            service
+                                .GetMonsterByIndex(index)
+                                .GetAwaiter()
+                                .GetResult();
 
-            try
-            {
-                Monster monsterByName = service.GetMonsterByName(name)
-                    .GetAwaiter()
-                    .GetResult();
+                        monster.DisplayInfos();
+                    }
+                    catch (HttpRequestException)
+                    {
+                        ConsoleHelper.WriteLineColor(
+                            "Index inconnu",
+                            ConsoleColor.DarkRed);
+                    }
 
-                monsterByName.DisplayInfos();
-            }
-            catch (MonsterNotFoundException ex)
-            {
-                ConsoleHelper.WriteLineColor(ex.Message, ConsoleColor.DarkRed);
+                    break;
+                }
+
+                case 2:
+                {
+                    Console.WriteLine("\nChoisissez un nom de monstre à consulter :");
+
+                    string name = Console.ReadLine() ?? "";
+
+                    try
+                    {
+                        Monster monster =
+                            service
+                                .GetMonsterByName(name)
+                                .GetAwaiter()
+                                .GetResult();
+
+                        monster.DisplayInfos();
+                    }
+                    catch (MonsterNotFoundException ex)
+                    {
+                        ConsoleHelper.WriteLineColor(
+                            ex.Message,
+                            ConsoleColor.DarkRed);
+                    }
+
+                    break;
+                }
+
+                case 9:
+                    break;
+
+                default:
+                    Console.WriteLine("Choix invalide");
+                    break;
             }
         }
     }
